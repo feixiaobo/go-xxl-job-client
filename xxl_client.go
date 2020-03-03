997375b3c70bd30edcb46cc7d10736872bca3eb9
feixiaobo <feixiaobo@wecash.net>
2020-03-03 17:16:25 +0800

    提交少了代码

--- xxl_client.go
+++ xxl_client.go
@@ -6,10 +6,7 @@ import (
 	"github.com/feixiaobo/go-xxl-job-client/admin"
 	"github.com/feixiaobo/go-xxl-job-client/handler"
 	"github.com/feixiaobo/go-xxl-job-client/logger"
-	"github.com/feixiaobo/go-xxl-job-client/utils"
-	"net/http"
-	"reflect"
-	"runtime"
+	"github.com/feixiaobo/go-xxl-job-client/server"
 	"time"
 )
 
@@ -46,108 +43,11 @@ func GetParam(ctx context.Context, key string) (val string, has bool) {
 	return "", false
 }
 
-func RequestHandler(buf []byte) (res []byte, err error) {
-	out := hessian.NewDecoder(buf)
-	r, err := out.Decode()
-
-	if r == nil || err != nil {
-		return nil, err
+func RunServer() {
+	if xxl.XxlAdmin.Port == 0 {
+		panic("executor must be init before run server")
 	}
-
-	response := xxl.XxlRpcResponse{}
-	returnt := xxl.ReturnT{
-		Code:    http.StatusOK,
-		Content: "success",
-	}
-
-	refMap := utils.RefletcStructToMap(r)
-	if refMap == nil {
-		returnt.Content = http.StatusInternalServerError
-		returnt.Content = "reflect request body error"
-	} else {
-		reqId := refMap["RequestId"].(string)
-		response.RequestId = reqId
-		if "BEAT_PING_PONG" != reqId { //处理非心跳请求
-			if xxl.XxlAdmin.AccessToken != "" && refMap["AccessToken"].(string) != xxl.XxlAdmin.AccessToken {
-				returnt.Content = http.StatusInternalServerError
-				returnt.Content = "access token error"
-			} else {
-				if refMap["MethodName"].(string) != "beat" {
-					req := r.(*xxl.XxlRpcRequest)
-					response.RequestId = req.RequestId
-					if len(req.Parameters) == 0 {
-						response.ErrorMsg = "job parameters is empty"
-						returnt.Code = http.StatusInternalServerError
-						returnt.Content = "job parameters is empty"
-					} else {
-						switch req.MethodName {
-						case "log":
-							fromLine := req.Parameters[2].(int32)
-							line, content := logger.ReadLog(req.Parameters[0].(int64), req.Parameters[1].(int64), fromLine)
-							log := logger.LogResult{
-								FromLineNum: fromLine,
-								ToLineNum:   line,
-								LogContent:  content,
-								IsEnd:       true,
-							}
-							returnt.Content = log
-						case "kill":
-							handler.CancelJob(req.Parameters[0].(int32))
-						default:
-							go pushJob(req.Parameters[0].(*xxl.TriggerParam))
-						}
-					}
-				}
-			}
-		}
-	}
-
-	response.Result = returnt
-	e := hessian.NewEncoder()
-	err = e.Encode(response)
-	if err != nil {
-		return nil, err
-	}
-
-	bytes := e.Buffer()
-	return bytes, nil
-}
-
-func getFunctionName(i interface{}) string {
-	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
-}
-
-func pushJob(trigger *xxl.TriggerParam) {
-	returnt := xxl.ReturnT{
-		Code:    http.StatusOK,
-		Content: "success",
-	}
-
-	fun, ok := handler.JobMap[trigger.ExecutorHandler]
-	if ok {
-		funName := getFunctionName(fun)
-		jobParam := &handler.JobRunParam{
-			LogId:       trigger.LogId,
-			LogDateTime: trigger.LogDateTime,
-			JobName:     trigger.ExecutorHandler,
-			JobFuncName: funName,
-			InputParam:  trigger.ExecutorParams,
-		}
-		if !handler.PushJob(trigger.JobId, jobParam) {
-			returnt.Code = http.StatusInternalServerError
-			returnt.Content = "push job queue failed"
-		}
-	} else {
-		returnt.Code = http.StatusInternalServerError
-		returnt.Content = "job handle not found"
-	}
-
-	callback := &xxl.HandleCallbackParam{
-		LogId:         trigger.LogId,
-		LogDateTim:    trigger.LogDateTime,
-		ExecuteResult: returnt,
-	}
-	xxl.CallbackAdmin([]*xxl.HandleCallbackParam{callback})
+	server.StartServer()
 }
 
 func RegisterJob(name string, function handler.JobHandlerFunc) {
